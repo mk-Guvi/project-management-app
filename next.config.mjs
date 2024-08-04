9e9e48cc370588d6fd60d63addff70fcc9c09e72
Mohammed Musharaf <mohammedm@zuper.local>
2024-08-04 16:26:06 +0530

    [WIP] fix cookies issue in production - updated config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
-};
-
-export default nextConfig;
+    reactStrictMode: false,
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Credentials',
+              value: 'true',
+            },
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: process.env.BACKEND_URL || 'localhost:1337', // Replace with your Render backend URL
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET,DELETE,PATCH,POST,PUT',
+            },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;

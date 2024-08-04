e93a701671c618380b2d739746c0f9526d51b78e
Mohammed Musharaf <mohammedm@zuper.local>
2024-08-04 15:59:24 +0530

    [FIX] redirect from home page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,33 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
-
+  const cookieStore = cookies();
   // Define auth routes (routes that don't require authentication)
-  const authRoutes = ['/login', '/signup'];
+  const authRoutes = ["/login", "/signup"];
 
   // Check for the presence of an access token in cookies
-  const hasAccessToken = req.cookies.has('accessToken');
-  const hasRefreshToken = req.cookies.has('refreshToken');
+  const hasAccessToken = cookieStore.has("accessToken");
+  const hasRefreshToken = cookieStore.has("refreshToken");
 
-  console.log("Cookies:", req.cookies);
-  console.log("Path:", path);
-  console.log("Has Access Token:", hasAccessToken);
-  console.log("Has Refresh Token:", hasRefreshToken);
+  console.log(
+    cookieStore.get("refreshToken"),
+    "Cookies:",
+    cookieStore.get("accessToken")
+  );
 
   // // If there's no access token and the user is not on an auth route, redirect to login
-  // if (!hasAccessToken && !hasRefreshToken && !authRoutes.includes(path)) {
-  //   console.log("Redirecting to /login");
-  //   return NextResponse.redirect(new URL('/login', req.url));
-  // }
+  if (!hasAccessToken && !hasRefreshToken && !authRoutes.includes(path)) {
+    console.log("Redirecting to /login");
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
-  // // If there is an access token and the user is on an auth route, redirect to home
-  // if ((hasAccessToken || hasRefreshToken) && authRoutes.includes(path)) {
-  //   console.log("Redirecting to /");
-  //   return NextResponse.redirect(new URL('/', req.url));
-  // }
+  // If there is an access token and the user is on an auth route, redirect to home
+  if ((hasAccessToken || hasRefreshToken) && authRoutes.includes(path)) {
+    console.log("Redirecting to /");
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 
   // For all other cases, allow access
   return NextResponse.next();
@@ -33,5 +35,5 @@ export function middleware(req: NextRequest) {
 
 // Specify which routes this middleware should run on
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };

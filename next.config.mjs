b5f8e6e049a6df9d64fc325551f5325e743ec71d
Mohammed Musharaf <mohammedm@zuper.local>
2024-08-04 16:48:56 +0530

    [WIP] fix cookies issue in production - updated config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'Access-Control-Allow-Credentials',
-              value: 'true',
-            },
-            {
-              key: 'Access-Control-Allow-Origin',
-              value: process.env.NEXT_PUBLIC_SERVER_ENDPOINT || 'localhost:1337', // Replace with your Render backend URL
-            },
-            {
-              key: 'Access-Control-Allow-Methods',
-              value: 'GET,DELETE,PATCH,POST,PUT',
-            },
-            {
-              key: 'Access-Control-Allow-Headers',
-              value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+  reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NEXT_PUBLIC_SERVER_ENDPOINT || "localhost:1337", // Replace with your Render backend URL
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token,X-Requested-With,Accept,Accept-Version,Content-Length,Content-MD5,Content-Type,Date,X-Api-Version,X-Api-Name",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
